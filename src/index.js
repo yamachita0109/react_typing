601d147669493b6ade8ddc4502ade3697752ec88
山下 数政 <yamashita-kazu@gnavi.co.jp>
2020-07-10 22:46:57 +0900

    FirstCommit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Question extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          {this.props.timer}
+        </div>
+        <div>
+          {/* {this.state.text} */}
+        </div>
+      </div>
+    )
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Anser extends React.Component {
+  
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timer: 3,
+      text: '祇園生者の鐘の音'
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const i = this.state.timer - 1
+      if (i === 0) {
+        clearInterval(this.interval)
+        return
+      }
+      this.setState({timer: i})
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.timer}
+        </div>
+        <div>
+          {this.state.text}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      start: false
+    }
+  }
+  click() {
+    this.setState({
+      start: !this.state.start
+    })
+  }
+  render() {
+    const status = this.state.start
+    const startButton = (<button onClick={() => this.click()}>Ready...</button>)
+    const content = (<Game />)
+  
+    return (
+      <div className='main'>
+        {status ? content : startButton}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'))
